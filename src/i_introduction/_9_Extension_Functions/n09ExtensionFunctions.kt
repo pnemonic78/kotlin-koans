2b82188c7571553fd245fcbef63836d4a23a8f8e
Moshe <moshe.waisberg@tikalk.com>
2017-08-30 19:49:19 +0300

    i_introduction._9_Extension_Functions

--- src/i_introduction/_9_Extension_Functions/n09ExtensionFunctions.kt
+++ src/i_introduction/_9_Extension_Functions/n09ExtensionFunctions.kt
@@ -1,5 +1,12 @@
 package i_introduction._9_Extension_Functions
 
+// declares an extension function that returns the last character
+fun String.lastChar() = this.get(this.length - 1)
+
+
+// 'this' refers to the receiver (String) and can be omitted
+fun String.lastChar1() = get(length - 1)
+
 data class RationalNumber(val numerator: Int, val denominator: Int)
 
 fun Int.r(): RationalNumber = RationalNumber(this, 1)
