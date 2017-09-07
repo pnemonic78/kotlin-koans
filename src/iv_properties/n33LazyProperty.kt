b01a371baa3325dc803434a996f7c4e53c47ba53
Moshe <moshe.waisberg@tikalk.com>
2017-09-07 18:08:39 +0300

    iv_properties/n34DelegatesExamples.kt

--- src/iv_properties/n33LazyProperty.kt
+++ src/iv_properties/n33LazyProperty.kt
@@ -1,5 +1,13 @@
 package iv_properties
 
 class LazyProperty(val initializer: () -> Int) {
-    val lazy: Int by lazy { initializer() }
+    private var didLazy: Boolean = false
+    val lazy: Int = 0
+        get() {
+            if (!didLazy) {
+                field = initializer()
+                didLazy = true
+            }
+            return field
+        }
 }
