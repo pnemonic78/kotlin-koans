f69f5097d654629130b2c7c6553780babe9e5acb
Moshe <moshe.waisberg@tikalk.com>
2017-09-10 10:25:08 +0300

    v_builders/n37StringAndMapBuilders.kt

--- src/v_builders/n37StringAndMapBuilders.kt
+++ src/v_builders/n37StringAndMapBuilders.kt
@@ -1,39 +1,18 @@
 package v_builders
 
-import util.TODO
 import java.util.*
 
-fun buildStringExample(): String {
-    fun buildString(build: StringBuilder.() -> Unit): String {
-        val stringBuilder = StringBuilder()
-        stringBuilder.build()
-        return stringBuilder.toString()
+fun task37(): Map<Int, String> {
+    fun buildMap(build: MutableMap<Int, String>.() -> Unit): Map<Int, String> {
+        val map = HashMap<Int, String>()
+        map.build()
+        return map
     }
 
-    return buildString {
-        this.append("Numbers: ")
+    return buildMap {
+        put(0, "0")
         for (i in 1..10) {
-            // 'this' can be omitted
-            append(i)
+            put(i, "$i")
         }
     }
-}
-
-fun todoTask37(): Nothing = TODO(
-    """
-        Task 37.
-        Uncomment the commented code and make it compile.
-        Add and implement function 'buildMap' with one parameter (of type extension function) creating a new HashMap,
-        building it and returning it as a result.
-    """
-)
-
-fun task37(): Map<Int, String> {
-    todoTask37()
-//    return buildMap {
-//        put(0, "0")
-//        for (i in 1..10) {
-//            put(i, "$i")
-//        }
-//    }
 }
